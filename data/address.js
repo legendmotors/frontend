1bb9691356bed5d882785e2afbebff7db47480ae
mohamedsahin <mohamedsahinva@gmail.com>
2025-03-12 11:48:54 +0400

    -

--- data/address.js
+++ data/address.js
@@ -5,7 +5,7 @@ export const branches = [
       email: "sales@legendmotorsuae.com",
       phone: "+971 4 548 5633",
       fax: "+971 4 333 3495",
-      mapSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3623.048780761619!2d55.40590731543237!3d25.1543949406579!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f5e6cbbfca18b%3A0x90b45094f2bc6c84!2sLegend%20Motors%2046%2C%20Dubai%201!5e0!3m2!1sen!2sae!4v1633006636379!5m2!1sen!2sae",
+      mapSrc: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14443.997020552924!2d55.368183!3d25.169503!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f67b797eb5a31%3A0xc0f1a9fbb4f88db5!2sLegend%20Motors%2046%2C%20Dubai%201!5e0!3m2!1sen!2sin!4v1741765023127!5m2!1sen!2sin",
     },
     {
       title: "LEGEND MOTORS DUBAI 2",
@@ -13,7 +13,7 @@ export const branches = [
       email: "sales@legendmotorsuae.com",
       phone: "+971 4 333 3420",
       fax: "+971 4 333 3495",
-      mapSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3623.048780761619!2d55.40590731543237!3d25.1543949406579!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f5e6cbbfca18b%3A0x90b45094f2bc6c84!2sLegend%20Motors%20116%2C%20Dubai%202!5e0!3m2!1sen!2sae!4v1633006636379!5m2!1sen!2sae",
+      mapSrc: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14443.35713668404!2d55.370154!3d25.174904!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f66e4baa4cdff%3A0x4a4915c6fc58ed5c!2sLegend%20Motors%20116%2C%20Dubai%202!5e0!3m2!1sen!2sin!4v1741765098461!5m2!1sen!2sin",
     },
     {
       title: "LEGEND MOTORS DUBAI 3",
@@ -21,7 +21,7 @@ export const branches = [
       email: "sales@legendmotorsuae.com",
       phone: "+971 4 320 7688",
       fax: "+971 4 320 7608",
-      mapSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3623.048780761619!2d55.40590731543237!3d25.1543949406579!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f5e6cbbfca18b%3A0x90b45094f2bc6c84!2sLegend%20Motors%20268%2C%20Dubai%203!5e0!3m2!1sen!2sae!4v1633006636379!5m2!1sen!2sae",
+      mapSrc: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d231103.8404994718!2d55.373836!3d25.169562!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f66ef1618a3dd%3A0xe54f3960d8f30591!2sLegend%20Motors%20268%2C%20Dubai%203!5e0!3m2!1sen!2sus!4v1741765132626!5m2!1sen!2sus",
     },
     {
       title: "LEGEND MOTORS DUBAI 4",
@@ -29,7 +29,15 @@ export const branches = [
       email: "sales@legendmotorsuae.com",
       phone: "+971 4 352 7688",
       fax: "+971 4 320 7608",
-      mapSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3623.048780761619!2d55.40590731543237!3d25.1543949406579!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f5e6cbbfca18b%3A0x90b45094f2bc6c84!2sLegend%20Motors%2026%2C%20Dubai%204!5e0!3m2!1sen!2sae!4v1633006636379!5m2!1sen!2sae",
+      mapSrc: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d231097.74258445727!2d55.376631!3d25.172779!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f678650c58d69%3A0xaa6cd74d7688f092!2sLegend%20Motors%2026%2C%20Dubai%204!5e0!3m2!1sen!2sus!4v1741765165978!5m2!1sen!2sus",
+    },
+    {
+      title: "LEGEND MOTORS JAFZA",
+      address: "Jebel Ali Freezone, Plot No. S30502 - opposite Redington, Gate5 - Mina Jebel Ali - Dubai",
+      email: "sales@legendmotorsuae.com",
+      phone: "+971 4 326 6463",
+      fax: "",
+      mapSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3616.9529226273226!2d55.1175978!3d24.9677163!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f0dd31c86fa39%3A0x95455816f372c894!2sLegend%20Motors!5e0!3m2!1sen!2sae!4v1741765567714!5m2!1sen!2sae",
     },
   ];
   
